402114fdfe0e8badeefc8d78962df6246279cb36
James Fraser-Jones <james@fraser-jones.co.uk>
2025-01-10 15:14:00 +0000

    added web components

--- src/App.svelte
+++ src/App.svelte
@@ -5,3 +5,11 @@
 <div class="text-center bg-blue-500 text-white p-4 rounded-lg">
   <h1 class="text-2xl">Hello, {name}!</h1>
 </div>
+
+<label>
+  Material 3
+  <md-checkbox checked></md-checkbox>
+</label>
+
+<md-outlined-button>Back</md-outlined-button>
+<md-filled-button>Next</md-filled-button>
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
-import { mount } from 'svelte'
-import './app.css'
-import App from './App.svelte'
+import { mount } from "svelte";
+import "./app.css";
+import App from "./App.svelte";
+import "@material/web/button/filled-button.js";
+import "@material/web/button/outlined-button.js";
+import "@material/web/checkbox/checkbox.js";
 
 const app = mount(App, {
-  target: document.getElementById('app')!,
-})
+  target: document.getElementById("app")!,
+});
 
-export default app
+export default app;
