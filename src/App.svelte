e12ea0c5199f70a8d3c8f7e933111ee8130d6f70
James Fraser-Jones <james@fraser-jones.co.uk>
2025-01-10 13:57:30 +0000

    added tailwind

--- src/App.svelte
+++ src/App.svelte
@@ -1,35 +1,7 @@
 <script lang="ts">
-  import tree from './assets/tree.svg'
-  import Counter from './lib/Counter.svelte'
+  let name = "World";
 </script>
 
-<main>
-  <div>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={tree} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Task Tree</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-</main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
+<div class="text-center bg-blue-500 text-white p-4 rounded-lg">
+  <h1 class="text-2xl">Hello, {name}!</h1>
+</div>
